044226741c07f1e78b8d65c040748f95fbc48dab
ishan086 <ishan086@gmail.com>
2018-05-18 23:55:31 -0400

    updates to deploy to heroku13e2

--- server.js
+++ server.js
@@ -5,5 +5,9 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/shopping-app/dist'));
 
+app.get('*', (req, res) => {
+    res.sendFile(`./shopping-app/index.html`); // load the single view file (angular will handle the page changes on the front-end)
+});
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
